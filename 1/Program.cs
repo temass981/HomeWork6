ddf97f63433fd9be3f0d4d9b8d2a2826f62ed369
Artyom <simstemasss@mail.ru>
2022-12-10 14:31:37 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+int [] array1 = Transformation(st);
+
+System.Console.WriteLine(String.Join(",",array1));
+
+Count(array1);
+
+int [] Transformation (string [] array){
+    int [] trans = new int [array.Length];
+    for (int i=0; i<array.Length; i++){
+        trans[i]=int.Parse(array[i]);
+    }
+    return trans;
+}
+
+void Count(int [] array){
+    int count = 0;
+    for (int i=0; i<array.Length; i++){
+        if (array[i]>0) count++;
+    }
+    System.Console.WriteLine(count);
+}
---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+
+void Intersection(double k1,double b1, double k2, double b2){
+    if((k1==k2) &&(b1==b2)) System.Console.WriteLine("Прямые совпадают");
+    else if (k1==k2) System.Console.WriteLine("Прямые параллельны");
+    else{
+   double x =(b2-b1)/(k1-k2);
+   
+   double y =(k1*(b2-b1))/(k1-k2)+b1;
+    System.Console.WriteLine(x+" ; "+y);
+}
+}
+Intersection(k1,b1,k2,b2);
